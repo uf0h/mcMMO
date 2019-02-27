1be5a04df6e235c7adba49c5f5a38a8ad950f12a
uf0h <3p6824cbrf@gmail.com>
2019-02-27 09:24:09 +0000

    merge fix for memory leak #3631
    https://github.com/mcMMO-Dev/mcMMO/pull/3631

--- src/main/java/com/gmail/nossr50/listeners/EntityListener.java
+++ src/main/java/com/gmail/nossr50/listeners/EntityListener.java
@@ -397,6 +397,10 @@ public class EntityListener implements Listener {
       entity.setCustomNameVisible(entity.getMetadata(mcMMO.customVisibleKey).get(0).asBoolean());
       entity.removeMetadata(mcMMO.customVisibleKey, plugin);
     }
+
+    if (entity.hasMetadata(mcMMO.entityMetadataKey)) {
+      entity.removeMetadata(mcMMO.entityMetadataKey, plugin);
+    }
   }
 
   /**
